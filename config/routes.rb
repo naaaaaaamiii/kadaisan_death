42dccea8c3f5d05985ef64259d2346c90e2b2371
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-06-16 09:55:22 +0000

    5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
   
+  resources :users, only: [:index, :show]
+  resources :books, only: [:new, :show, :create, :index, :edit]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20230616095022_add_user_id_from_books.rb
@@ -0,0 +1,4 @@
+class AddUserIdFromBooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
